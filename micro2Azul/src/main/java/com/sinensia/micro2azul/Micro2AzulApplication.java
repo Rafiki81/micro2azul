b70b0d7b9b92b49c4788dd5ef5362899b560d834
rafa <rperezbeato@gmail.com>
2020-09-01 12:29:54 +0200

    Commit inicial, creacion del proyecto.

---
+++ micro2Azul/src/main/java/com/sinensia/micro2azul/Micro2AzulApplication.java
@@ -0,0 +1,13 @@
+package com.sinensia.micro2azul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro2AzulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro2AzulApplication.class, args);
+	}
+
+}
---
+++ micro2Azul/src/test/java/com/sinensia/micro2azul/Micro2AzulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinensia.micro2azul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micro2AzulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
